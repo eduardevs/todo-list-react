983bece631ccae4c2c68e3441d201b977a738f46
Eduardo Pina <jeseduardopi@gmail.com>
2022-03-16 11:04:17 +0100

    Add styles and text on check toggle

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,18 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 
 export default function Item({ title, content, onRemove, item }) {
+    const [check, setCheck] = useState(false)
+
+    const handleCheck = () => {
+        setCheck(prevCheck => !prevCheck)
+    }
 
     return (
-        <div className="card mb-3 col-10 col-md-6 col-lg-4">
-            <div className="card-header">
-                <h3 className="card-title">{title}</h3>
+        <div className="card mb-3 col-10 col-md-6 col-lg-4" style={{
+            color: !check ? "black" : "grey"
+        }}>
+            <div className="card-header row justify-content-between g-0" style={{
+                backgroundColor: !check ? "whitesmoke" : "white"
+            }
+            }>
+                <h3 className={!check ? "col" : "text-decoration-line-through col"}>{title}</h3>
+                <div className="col row justify-content-between">
+                    <label className="text-center">Mark{!check ? " finished" : " unfinished"}
+                    </label>
+                    <input type="checkbox" className="btn btn-success" onClick={handleCheck} />
+                </div>
             </div>
+
             <div className="card-body">
-                <h6>Details :</h6>
-                <p className="p-2">{content}</p>
+                <h6 className={!check ? "" : "text-decoration-line-through"}>Details :</h6>
+                <p className={!check ? "p-2" : "text-decoration-line-through p-2"}>{content}</p>
                 <div class="d-flex justify-content-around">
-                    <button type="button" class="btn btn-danger" onClick={() => onRemove(item.id)}>Delete</button>
+                    <button type="button" className="btn btn-danger" onClick={() => onRemove(item.id)}>Delete</button>
                 </div>
             </div>
         </div >
